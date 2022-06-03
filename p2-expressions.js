0d34344933cc89a6b6d995e6fd7d61823a771616
qiqima <105311675+qiqima@users.noreply.github.com>
2022-06-03 13:42:50 -0700

    Add files via upload

---
+++ p2-expressions.js
@@ -0,0 +1,43 @@
+/*
+    CIT 281 Project 2
+    Name: Qiqi Ma
+*/
+
+const getRandomInteger = function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+const alph = "abcdefghijklmnopqrstuvwxyz".split("");
+let result = "";
+let lengthOfOutputString = getRandomInteger(5, 26);
+for (let i = 0; i < lengthOfOutputString; i++) {
+    result += alph[getRandomInteger(0,alph.length)];
+};
+console.log(result);
+
+//function getRandomLetter() that will return a single, random, lowercase letter.
+const getRandomLetter = function(){
+    const alph = 'abcdefghijklmnopqrstuvwxyz';
+    let randomLetter = (alph.charAt(Math.floor(Math.random() * (alph.length))));
+    return randomLetter;
+};
+for (let i = 0; i < 1; i++) {
+    console.log(getRandomLetter());
+};
+
+//function getRandomString(minLength, maxLength) that will return the random length string.
+const getRandomString = function(minLength, maxLength) {
+  let randomString = getRandomInteger(minLength, maxLength);
+  let result = "";
+  for (let i = 0; i < randomString; i++) {
+      result += getRandomLetter();
+  }
+  return result;
+};
+console.log(getRandomString(10,20));
+
+//function getSortedString(string) that will return a string in ascending order (i.e., xpacd would return acdpx)
+ const getSortedString = function(){
+  return getRandomString(10,20).split("").sort().join("");
+}
+console.log(getSortedString());
